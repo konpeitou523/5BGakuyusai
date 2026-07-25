b81f8b85eea9ce66e0f93cbad92ad498249e7472
konpeitou523 <takahashikai523@gmail.com>
2026-07-25 21:07:29 +0900

    table、reservationに関して一旦完成(他ページに飛ばすのはまだ)

--- src/controllers/reservationController.js
+++ src/controllers/reservationController.js
@@ -6,19 +6,33 @@ import {
 export async function sendReservationForm(req, res) {
   const reservations = await getReservations();
   const { timeschedule } = req.app.locals;
-  let { time } = req.query;
-  if (time in timeschedule){}else{
+  const { maxpeople } = req.app.locals;
+  let { time = "09:00" } = req.query;
+  if (!(time in timeschedule)) {
     console.log(`Warning 不正な入力${time}`);
-    time="09:00"
+    time = "09:00";
   }
 
   res.render("reservation.ejs", {
     reservations: reservations,
-    selectedtime:time
+    selectedtime: time,
   });
 }
 export async function createReservation(req, res) {
+  const { timeschedule, maxpeople } = req.app.locals;
   const { name, time, people } = req.body;
-  await insertReservation(name, time, people);
-  res.redirect("/reservation");
+  const peopleNum = Number(people);
+  const nameStr = String(name);
+  if (
+    nameStr !== "" &&
+    time in timeschedule &&
+    peopleNum >= 1 &&
+    peopleNum <= maxpeople
+  ) {
+    await insertReservation(name, time, people);
+    res.redirect("/reservation");
+  } else {
+    console.log(`不正な入力${name}${time}${people}`);
+    res.redirect("/reservation");
+  }
 }
