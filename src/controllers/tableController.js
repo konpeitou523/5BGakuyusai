33600366c033b10cabddf6ced960ab06e248ad56
konpeitou523 <takahashikai523@gmail.com>
2026-07-25 16:35:15 +0900

    table.ejsにて予約状況を取得し時間帯別に表示するよう調整

--- src/controllers/tableController.js
+++ src/controllers/tableController.js
@@ -3,7 +3,22 @@ import { getReservations } from "../models/reservationModel.js";
 export async function sendTable(req, res) {
   const reservations = await getReservations();
   const { interval } = req.app.locals;
+  const schedule = {};
+  for (let i = 540; i <= 820; i = i + interval) {
+    const hour = String(Math.floor(i / 60)).padStart(2, "0");
+    const minute = String(i % 60).padStart(2, "0");
+    schedule[`${hour}:${minute}`] = 0;
+  }
+
+  reservations.forEach((reservation) => {
+    if (reservation.time in schedule) {
+      schedule[reservation.time] += reservation.people;
+    } else {
+      console.log("存在しない時間帯の予約が入っています");
+    }
+  });
   res.render("table.ejs", {
     reservations: reservations,
+    schedule:schedule,
   });
 }
