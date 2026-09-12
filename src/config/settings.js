8e60ac7e193bce1b55ddfaca5f0b749e30287aaa
konpeitou523 <takahashikai523@gmail.com>
2026-09-12 15:12:41 +0900

    1日目に一時変更

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,12 +1,12 @@
 export const interval = 25;
-export const maxpeople = 20;
+export const maxpeople = 10;
 const schedule = {};
-const list=[];
-for (let i = 540; i <= 830-interval; i = i + interval) {
+const list = [];
+for (let i = 540; i <= 830 - interval; i = i + interval) {
   const hour = String(Math.floor(i / 60)).padStart(2, "0");
   const minute = String(i % 60).padStart(2, "0");
   schedule[`${hour}:${minute}`] = 0;
   list.push(`${hour}:${minute}`);
 }
-export const timeschedule=schedule;
-export const timelist=list;
+export const timeschedule = schedule;
+export const timelist = list;
